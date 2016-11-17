950f2cd890e3ddc1289507ae619e454c87cfe237
JonathanStevens17 <7JStevens@stpatrick.org>
2016-11-17 10:52:48 -0600

    WPA

---
+++ pizza.js
@@ -0,0 +1,19 @@
+class pizza{
+  //type constructor below
+  constructor(size, meatToppings, veggieToppings){
+    this.size = s;
+    this.meatToppings = mT;
+    this.veggieToppings = vT;
+
+
+
+  //type instance functions below
+sizeCost(){
+  
+}
+
+
+  //type class functions below
+
+
+}

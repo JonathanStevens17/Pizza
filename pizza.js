ed8e06da89cc545639b7e47f03db0f1acc819feb
JonathanStevens17 <7JStevens@stpatrick.org>
2016-11-18 10:52:09 -0600

    WIP

--- pizza.js
+++ pizza.js
@@ -9,11 +9,35 @@ class pizza{
 
   //type instance functions below
 sizeCost(){
-  
+  if(this.size == "Small"){
+    return 7.99;
 }
 
+  if(this.size == "Medium"){
+    return sizeCost() 9.99;
+}
 
+  if(this.size == "Large"){
+    return sizeCost() 12.99;
+}
+
+  if(this.size == "Extra-Large"){
+    return sizeCost() 15.99;
+}
   //type class functions below
+toppingCost(top){
+  if(top == meat){
+    return this.meatToppings.length*.99
+}
+
+  if(top == veggie){
+    return this.veggieToppings.length*.50
+}
+
+price(){
+  return
+
+
 
 
 }
